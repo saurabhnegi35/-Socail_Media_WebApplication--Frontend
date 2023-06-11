b3d3079f027be23bb5ff8bcbf05c46fe1c5cb853
SAURABH NEGI <saurabhnegi.35@gmail.com>
2023-06-11 10:25:24 +0530

    Setup Folder Structure and Routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "views/homePage";
+import LoginPage from "views/loginPage";
+import ProfilePage from "views/profilePage";
 
 function App() {
   return (
     <div className="app">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
